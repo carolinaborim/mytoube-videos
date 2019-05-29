da1505a6d9b2d7d14b60a33e2a9318ed0d8c8ecd
Carolina Borim <carolina.borim@gmail.com>
2019-05-29 19:34:43 +0200

    Adds an iframe to proper show the video.

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,8 +3,21 @@ import React from "react";
 class VideoDetail extends React.Component {
   render() {
     const video = this.props.video;
-    return video ? <div>{video.snippet.title}</div> : <div>Loading</div>;
+    const videoSrc = "https://www.youtube.com/embed/";
+
+    return video ? (
+      <div>
+        <div className="ui embed">
+          <iframe src={videoSrc + video.id.videoId} />
+        </div>
+        <div className="ui segment">
+          <h4 className="ui header">{video.snippet.title}</h4>
+          <p>{video.snippet.description}</p>
+        </div>
+      </div>
+    ) : (
+      <div>Loading</div>
+    );
   }
 }
-
 export default VideoDetail;
